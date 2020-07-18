99fdce3318175b4a98363fae00304c4b1ea06dbb
k0r1 <minhquan.k0r1@gmail.com>
2020-07-18 23:46:36 +0700

    Update gitlab cicd

--- deploy/gitlab/.gitlab-deploy-dev.sh
+++ deploy/gitlab/.gitlab-deploy-dev.sh
@@ -5,8 +5,8 @@ string=$PROD_DEPLOY_SERVER
 array=(${string//,/})
 for i in "${!array[@]}"; do 
     echo "Deploy project on server ${array[i]}"
-    ssh ubuntu@${array[i]} "ls -la golang-testing-devops/"
-    ssh ubuntu@${array[i]} "git -C golang-testing-devops pull origin master"
-    ssh ubuntu@${array[i]} "sudo docker-compose -f golang-testing-devops/docker-compose.yml down"
-    ssh ubuntu@${array[i]} "sudo docker-compose -f golang-testing-devops/docker-compose.yml up -d"
+    ssh ubuntu@${array[i]} "ls -la devops-golang/"
+    ssh ubuntu@${array[i]} "sudo git -C devops-golang pull origin master"
+    ssh ubuntu@${array[i]} "sudo docker-compose -f devops-golang/docker-compose.yml down"
+    ssh ubuntu@${array[i]} "sudo docker-compose -f devops-golang/docker-compose.yml up -d"
 done
