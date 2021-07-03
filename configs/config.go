e8d5c42d0a4b03ae92c6d3f2b3182abfee99485a
k0r1 <minhquan.k0r1@gmail.com>
2021-07-03 20:40:01 +0700

    Remvoe s3 and mail

--- configs/config.go
+++ configs/config.go
@@ -9,50 +9,21 @@ const (
 	ModeProduction = "prod"
 )
 
-type MailConfig struct {
-	Sender    string
-	Password  string
-	Host      string
-	Port      string
-	Receivers []string
-}
-
-type AWSConfig struct {
-	AccessKeyID     string
-	SecretAccessKey string
-	Region          string
-}
 type EnvConfig struct {
 	ServerMode      string
 	Port            string
 	DatabaseUri     string
 	DatabaseProject string
 	JWTKey          string
-	Mail            MailConfig
-	AWS             AWSConfig
 }
 
 func Load() EnvConfig {
 	cfg := EnvConfig{
-		ServerMode: os.Getenv("SERVER_MODE"),
-		Port:       os.Getenv("APP_PORT"),
-		Mail: MailConfig{
-			Sender:   os.Getenv("MAIL_SENDER"),
-			Password: os.Getenv("MAIL_PASSWORD"),
-			Host:     os.Getenv("MAIL_HOST"),
-			Port:     os.Getenv("MAIL_PORT"),
-			Receivers: []string{
-				os.Getenv("MAIL_RECEIVER"),
-			},
-		},
+		ServerMode:      os.Getenv("SERVER_MODE"),
+		Port:            os.Getenv("APP_PORT"),
 		DatabaseUri:     os.Getenv("DATABASE_URI"),
 		DatabaseProject: os.Getenv("DATABASE_PROJECT"),
 		JWTKey:          os.Getenv("JWT_KEY"),
-		AWS: AWSConfig{
-			AccessKeyID:     os.Getenv("AWS_ACCESS_KEY_ID"),
-			SecretAccessKey: os.Getenv("AWS_SECRET_ACCESS_KEY"),
-			Region:          os.Getenv("AWS_REGION"),
-		},
 	}
 
 	return cfg
